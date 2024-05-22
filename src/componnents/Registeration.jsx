cc01aa058fcb01c84fa93ed95b080a9ff0a93359
liav2002 <liavmail00@gmail.com>
2024-05-22 15:14:45 +0300

    improve register and login feature.

--- src/componnents/Registeration.jsx
+++ src/componnents/Registeration.jsx
@@ -68,111 +68,70 @@ function Registeration(props) {
         <Modal className='modal-lg' show={showModal} onHide={() => setShowModal(false)}>
           <Modal.Header closeButton>
             <Modal.Title id="authModalLabel">
-              <Button variant="btn btn-warning" style={{ color: "white", border: "none" }} onClick={handleToggle}>
+              <Button variant="btn btn-warning" style={{color: "white", backgroundColor: "#ea6ea6", border: "none" }} onClick={handleToggle}>
                 {isSignIn ? 'SIGN UP' : 'SIGN IN'}
               </Button>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <section className={isSignIn?"vh-70" :"vh-100"}style={{ backgroundColor: '#eee' } }>
+            <section className={isSignIn ? "vh-70" : "vh-100"} style={{ backgroundColor: '#f9f7f7' }}>
               <div className="container">
                 <div className="row d-flex justify-content-center align-items-center">
                   <div className="col-lg-12 col-xl-11">
-                    <div className="card text-black" style={{ borderRadius: '25px' }}>
+                    <div className="card" style={{ borderRadius: '25px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
                       <div className="card-body p-md-5">
                         <div className="row justify-content-center">
                           <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                             <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                              {isSignIn ? 'Sign In' : 'Sign up'}
+                              {isSignIn ? 'Sign In' : 'Sign Up'}
                             </p>
                             <form className="mx-1 mx-md-4">
-                              {!isSignIn ? (
-                                <div>
-                                  <div className="d-flex flex-row align-items-center mb-4">
-                                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                                    <div className="form-outline flex-fill mb-0">
-                                      <input type="text" id="signUp_username" className="form-control" />
-                                      <label className="form-label" htmlFor="signUp_username">
-                                        Username
-                                      </label>
-                                    </div>
-                                  </div>
-                                </div>
-                              ) : (
-                                <div>
-                                  <div className="d-flex flex-row align-items-center mb-4">
-                                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                                    <div className="form-outline flex-fill mb-0"> 
-                                      <input type="text" id="signIn_Username" className="form-control" />
-                                        <label className="form-label" htmlFor="signIn_Username">
-                                          Username
-                                        </label>
-                                    </div>
-                                  </div>
-                                </div>
-                                )
-                              }
                               <div className="d-flex flex-row align-items-center mb-4">
-                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                {!isSignIn ? (
-                                  <div className="form-outline flex-fill mb-0"> 
+                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                  <input type="text" id={isSignIn ? "signIn_Username" : "signUp_username"} className="form-control" />
+                                  <label className="form-label" htmlFor={isSignIn ? "signIn_Username" : "signUp_username"}>
+                                    Username
+                                  </label>
+                                </div>
+                              </div>
+                              {!isSignIn && (
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                  <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                  <div className="form-outline flex-fill mb-0">
                                     <input type="email" id="signUp_Email" className="form-control" />
                                     <label className="form-label" htmlFor="signUp_Email">
                                       Email
                                     </label>
                                   </div>
-                                ) : null}
-                              </div>
+                                </div>
+                              )}
                               <div className="d-flex flex-row align-items-center mb-4">
                                 <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                {!isSignIn ? (
-                                  <div className="form-outline flex-fill mb-0">
-                                    <input type="password" id="signUp_Password" className="form-control" />
-                                    <label className="form-label" htmlFor="signUp_Password">
-                                      Password
-                                    </label>
-                                  </div>) : (
-                                    <div className="form-outline flex-fill mb-0">
-                                      <input type="password" id="signIn_Password" className="form-control" />
-                                      <label className="form-label" htmlFor="signIn_Password">
-                                        Password
-                                      </label>
-                                    </div>
-                                  )
-                                }
+                                <div className="form-outline flex-fill mb-0">
+                                  <input type="password" id={isSignIn ? "signIn_Password" : "signUp_Password"} className="form-control" />
+                                  <label className="form-label" htmlFor={isSignIn ? "signIn_Password" : "signUp_Password"}>
+                                    Password
+                                  </label>
+                                </div>
                               </div>
-                              {!isSignIn ? (
-                                  <>
-                                    <div className="d-flex flex-row align-items-center mb-4">
-                                      <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                                      <div className="form-outline flex-fill mb-0">
-                                        <input type="password" id="signUp_confirmPassword" className="form-control" />
-                                        <label className="form-label" htmlFor="signUp_confirmPassword">
-                                          Repeat Password
-                                        </label>
-                                      </div>
-                                    </div>
-                                  </>
-                              ) : null}
+                              {!isSignIn && (
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                  <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                  <div className="form-outline flex-fill mb-0">
+                                    <input type="password" id="signUp_confirmPassword" className="form-control" />
+                                    <label className="form-label" htmlFor="signUp_confirmPassword">
+                                      Repeat Password
+                                    </label>
+                                  </div>
+                                </div>
+                              )}
                               <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                {isSignIn ? (
-                                    <Button variant="btn btn-warning" style={{ color: "white", border: "none" }} type="submit" onClick={signIn} className="btn btn-primary btn-lg">
-                                      Sign in
-                                    </Button>
-                                  ) : (
-                                    <Button variant="btn btn-warning" style={{ color: "white", border: "none" }} type="submit" onClick={signUp} className="btn btn-primary btn-lg">
-                                      Register
-                                    </Button>
-                                )}
+                                <Button variant="btn btn-warning" style={{ color: "white", backgroundColor: "#ea6ea6", border: "none" }} type="submit" onClick={isSignIn ? signIn : signUp} className="btn btn-primary btn-lg">
+                                  {isSignIn ? 'Sign In' : 'Register'}
+                                </Button>
                               </div>
                             </form>
-                          </div>
-                          <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                              className="img-fluid"
-                              alt="sign in sign up"
-                            />
                           </div>
                         </div>
                       </div>
