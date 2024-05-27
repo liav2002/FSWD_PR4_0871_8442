2d3f84d479eee3bdf82a4fd5eebb6592c96fc1ad
liav2002 <liavmail00@gmail.com>
2024-05-27 19:27:39 +0300

    design game board.

--- src/componnents/GameManager.jsx
+++ src/componnents/GameManager.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useRef } from "react";
 import Registeration from "./Registeration"
+import Board from "./Board";
+
+const MinNumber = 75;
+const maxNumber = 95;
 
 function GameManager() {
     const [currentGames, setCurrentGames] = useState([]);
-    const [initialized, setInitialized] = useState(false);
     const [isGameStart, setIsGameStart] = useState(false);
-    const isWin = useRef(false);
+    const isWinGame = useRef(false);
 
     const quitGame = (index) => {
+        isWinGame.current = false;
+        setCurrentGames((prevGames) => prevGames.filter((game, i) => i !== index));
       };
 
     const addPlayer = (player) => {
@@ -18,17 +23,92 @@ function GameManager() {
     };
 
     const startGame = () => {
+        isWinGame.current = false;
+        setCurrentGames((prevGames) =>
+            prevGames.map((game, i) => ({
+                ...game,
+                disable: i === 0 ? false : true,
+                number: Math.floor(Math.random() * (maxNumber - MinNumber) + MinNumber),
+                isWin: false,
+                numberOfSteps: 0,
+            }))
+        );
+        setIsGameStart(true);
     };
 
     function winGame(game) {
-        return;
+        if (!game.isWin) {
+            let numberOfSteps = game.numberOfSteps + 1;
+            let number = game.number + 1;
+            let players = JSON.parse(localStorage.getItem("players")) || [];
+            let player = game.player;
+            let playerIndex = players.findIndex(
+              (storagePlayer) => storagePlayer.email === player.email
+            );
+            if (playerIndex !== -1) {
+              players[playerIndex].AllScores = [
+                ...players[playerIndex].AllScores,
+                numberOfSteps,
+              ];
+              localStorage.setItem("players", JSON.stringify(players));
+            }
+            return {
+              player: player,
+              number: number,
+              numberOfSteps: numberOfSteps,
+              isWin: true,
+            };
+          }
+          return game;
       }
+
+    const isOnePlayer = () => currentGames.length == 1;   
     
     const handleMove = (move, index) => {
+        setCurrentGames((prevGames) =>
+            prevGames.map((game, i) => {
+              if (i === index) {
+                const newNumber = eval(`${game.number} ${move}`);
+                const isWin = newNumber === 100;
+                if (isWin && !isWinGame.current) {
+                    isWinGame.current = true;
+                  return winGame(game);
+                } else {
+                  return {
+                    ...game,
+                    disable: !isOnePlayer(),
+                    number: newNumber,
+                    numberOfSteps: game.numberOfSteps + 1,
+                    isWin: isWin,
+                  };
+                }
+              } else if ((index + 1) % prevGames.length === i) {
+                return { ...game, disable: false };
+              } else {
+                return game;
+              }
+            })
+          );
     };
 
     const handleNewGame = (gameToStart) => {
-    };
+        isWinGame.current = false;
+        setCurrentGames((prevGames) =>
+          prevGames.map((game) =>
+            game === gameToStart
+              ? {
+                  ...game,
+                  disable: !isOnePlayer(),
+                  number: Math.floor(
+                    Math.random() * (maxNumber - MinNumber) + MinNumber
+                  ),
+                  isWin: false,
+                  numberOfSteps: 0,
+                }
+              : game
+          )
+        );
+      };
 
     return (
         <>
@@ -48,7 +128,18 @@ function GameManager() {
                     </div>
 
                     <div className="game-board">
-                        <h3>Game Board will be shown here</h3>
+                        {currentGames.map((game, index) => (
+                            <Board
+                                className="col-4"
+                                key={index}
+                                index={index}
+                                game={game}
+                                handleMove={handleMove}
+                                currentGames={currentGames}
+                                quitGame={quitGame}
+                                handleNewGame={handleNewGame}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
